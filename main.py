6c0a65b50d65c5def4105c025b8e945593260f5e
liav22 <40798583+liav22@users.noreply.github.com>
2019-11-26 12:58:57 +0200

    Initial

---
+++ main.py
@@ -0,0 +1,212 @@
+import asyncio
+import discord
+import time
+import subprocess
+import datetime
+
+from user_data import *
+from website_data import *
+
+TOKEN = 'NTQyNjc3NDgwMjk3ODU3MDM2.XU7K4g.58_z6prtMGt6pdrwlHb5_Pose4w' # DO NOT SHARE
+
+P = '~'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return # Ensuring the bot won't respond to himself
+
+	if message.author.bot:
+		return # Ensuring the bot won't respond to other bots
+
+	if message.content == P+'help' or message.content == P+'h':
+		embed = discord.Embed(title='Source code', url='https://gist.github.com/liav22/c587b6ba1c07b0ce8e2d64fa111642f4', colour=0x1e90ff, provider='me')
+		embed.add_field(name='General Commands:', value="""
+			Search Profile: `{p}user PSN` | Shortcut: `{p}u`
+			Search Prices: `{p}price Game` | Shortcut: `{p}p`
+			Search Trophies: `{p}trophy Game` | Shortcut: `{p}t`
+			Search Scores: `{p}meta Game` | Shortcut: `{p}m`
+			Search Length: `{p}hltb Game` | Shortcut `{p}h`""".format(p=P), inline=True)
+		embed.add_field(name='User Specific Commands:', value="""
+			register Me: `{p}register PSN`
+			Show My Profile: `{p}u`
+			Remove Me: `{p}unregister`
+			My Last Platinum: `{p}mlp`""".format(p=P), inline=True)
+		embed.set_author(name='Playstation Network Bot', icon_url='https://www.playstation.com/en-gb/1.36.45/etc/designs/pdc/clientlibs_base/images/nav/avatar-default-2x.png')
+		embed.set_footer(text='© Made by Liav22')
+		await message.channel.send(embed=embed)
+
+	if message.content.lower().startswith(P+'register '):
+
+		if message.guild is None:
+			await error_message(message.channel, 'Registration is only possible on servers.', 'Try using on a server where the bot is used.', '001')
+
+		if search_user(message.author.id, message.guild.id) == False:
+			register_new_user(message.author.id,message.content[10::], message.guild.id)
+			await message.channel.send(f'<@{message.author.id}> registered successfully.')
+
+		else:
+			await error_message(message.channel, 'User already registered.', 'Try `~u` or `~register`', '002')
+
+	if message.content.lower() == P+'register':
+		await error_message(message.channel, 'No username inserted.', 'Try `~register [USERNAME]`', '003')
+
+	if message.content.lower() == P+'unregister':
+		if search_user(message.author.id, message.guild.id) == True:
+			remove_user(message.author.id, message.guild.id)
+			await message.channel.send(f'<@{message.author.id}> removed successfully.')
+		else:
+			await error_message(message.channel, 'User not registered.', 'Use `~register [USERNAME]`', '004')
+
+	if message.content.lower() == (P+'u'):
+		if search_user(message.author.id, message.guild.id) == True:
+			try:
+				url = 'https://psnprofiles.com/' + lookup_user(message.author.id, message.guild.id)
+				soup = GetUserSoup(url)
+				a = UserInfo(soup)
+				embed = discord.Embed(description=a.description(), colour=0x4BA0FF)
+				embed.set_author(name=a.name() + "'s Profile", url=url, icon_url=a.icon())
+				embed.set_image(url=a.card())
+				embed.set_footer(text='by PSNProfiles.com')
+				await message.channel.send(embed=embed)
+
+			except AttributeError:
+				await error_message(message.channel, 'User not found on PSNProfiles.', 'Use `~unregister` and `~register [USERNAME]` again.', '005')
+
+		else:
+			await error_message(message.channel, 'User not registered.', 'Use `~register [USERNAME]`', '006')
+
+	if message.content.lower() == (P+'mlp') or message.content.lower() == (P+'mylastplatinum'):
+		if search_user(message.author.id, message.guild.id) == True:
+			url = 'https://psnprofiles.com/' + lookup_user(message.author.id, message.guild.id)
+			soup = GetUserSoup(url)
+			a = PlatinumInfo(soup)
+			embed = discord.Embed(title = a.game() + ' • ' + a.rarity(),description=a.description(), color=0x057fcc)
+			embed.set_author(name=a.name() + "'s last Platinum Trophy", url=url, icon_url='https://psnprofiles.com/lib/img/icons/40-platinum.png')
+			embed.set_thumbnail(url=a.image())
+			embed.set_footer(text='by PSNProfiles.com')
+			await message.channel.send(embed=embed)
+
+		else:
+			await error_message(message.channel, 'User not registered.', 'Use `~register [USERNAME]`', '006')
+
+	if message.content.lower().startswith(P+'u ') or message.content.lower().startswith(P+'user '):
+		if message.content.lower().startswith(P+'u '):
+			url = 'https://psnprofiles.com/' + message.content[3::]
+		if message.content.lower().startswith(P+'user '):
+			url = 'https://psnprofiles.com/' + message.content[6::]
+		soup = GetUserSoup(url)
+		a = UserInfo(soup)
+		embed = discord.Embed(description=a.description(), colour=0x4BA0FF)
+		embed.set_author(name=a.name() + "'s Profile", url=url, icon_url=a.icon())
+		embed.set_image(url=a.card())
+		embed.set_footer(text='by PSNProfiles.com')
+		await message.channel.send(embed=embed)
+
+
+	if message.content.lower().startswith(P+'trophy ') or message.content.lower().startswith(P+'t '):
+		if message.content.lower().startswith(P+'t '):
+			game = message.content[3::]
+		if message.content.lower().startswith(P+'trophy '):
+			game = message.content[8::]
+		try:
+			soup = GetTrophiesSoup(game)
+			a = TrophiesInfo(soup)
+			embed = discord.Embed(title=a.trophies()+a.comp(), description=a.guide(), colour=0x4BA0FF)
+			embed.set_author(name=a.name(), url=a.url(), icon_url='https://psnprofiles.com/lib/img/icons/logo-round-160px.png')
+			embed.set_image(url=a.image())
+			embed.set_footer(text='by PSNProfiles.com')
+			await message.channel.send(embed=embed)
+
+		except AttributeError:
+			await error_message_with_url(message.channel, 'Game not found.', 'Press the link to search for it yourself.`', f"https://psnprofiles.com/search/games?q={game.replace(' ','+')}", '008')
+
+		except Exception:
+			await error_message(message.channel, "Unknown Error; Google has probably blocked us, or it's currently down.", "Wait an hour and try again or inform the bot's developer.", '009')
+
+	if message.content.lower().startswith(P+'price ') or message.content.lower().startswith(P+'p '):
+		if message.content.lower().startswith(P+'p '):
+			game = message.content[3::]
+		if message.content.lower().startswith(P+'price '):
+			game = message.content[7::]
+
+		try:
+			soup = FindGame(game)
+			a = PriceInfo(soup)
+			embed = discord.Embed(title='Current Price: ' + a.price() + a.plus_price(), description='Lowest Price: ' + a.lowest_price(), url=a.page_url(), colour=0x2200FF)
+			embed.set_author(name=a.title(), url=a.store_url(a), icon_url='https://psprices.com/staticfiles/i/content__game_card__price_plus.bccff0c297cd.png')
+			embed.set_thumbnail(url=a.image())
+			embed.set_footer(text='by PSprices.com')
+			await message.channel.send(embed=embed)
+
+		except AttributeError:
+			await error_message_with_url(message.channel, 'Game not found.', 'Press the link to search for it yourself.', f"https://psprices.com/region-us/search/?q={game.replace(' ','+')}&dlc=show&platform=PS4", '010')
+
+		except Exception:
+			await error_message(message.channel, "Unknown Error.", "Inform the bot's developer.", '007')
+
+	if message.content.lower().startswith(P+'meta ') or message.content.lower().startswith(P+'m '):
+		if message.content.lower().startswith(P+'m '):
+			game = message.content[3::]
+		if message.content.lower().startswith(P+'meta '):
+			game = message.content[6::]
+
+		try:
+			soup = FindMetaSoup(game)
+			a = MetaInfo(soup)
+			embed = discord.Embed(title='Metascore: ' + a.score() + ', by ' + a.critics(), description=a.quote(), colour=a.color())
+			embed.set_author(name=a.title(), url=a.url(), icon_url='https://i.imgur.com/jpgFaHq.png')
+			embed.set_thumbnail(url=a.image())
+			embed.set_footer(text='by Metacritic.com')
+			await message.channel.send(embed=embed)
+
+		except AttributeError:
+			await error_message_with_url(message.channel, 'Game not found.', 'Press the link to search for it yourself.', f"https://www.metacritic.com/search/game/{game.replace(' ','+')}/results", '011')
+
+		except Exception:
+			await error_message(message.channel, "Unknown Error: Google has either blocked us, or it's currently down.", "Wait an hour and try again or inform the bot's developer.", '009')
+
+	if message.content.lower().startswith(P+'hltb ') or message.content.lower().startswith(P+'h '):
+		if message.content.lower().startswith(P+'h '):
+			game = message.content[3::]
+		if message.content.lower().startswith(P+'hltb '):
+			game = message.content[6::]
+
+		try:
+			soup = HowLongSoup(game)
+			a = HowLongInfo(soup)
+			embed = discord.Embed(description=a.times(), colour=0x328ED)
+			embed.set_author(name=a.title(), url=a.url(), icon_url='https://i.imgur.com/WjDVkDF.jpg')
+			embed.set_thumbnail(url=a.image())
+			embed.set_footer(text='by HowLongToBeat.com')
+			await message.channel.send(embed=embed)
+			
+		except AttributeError:
+			await error_message_with_url(message.channel, 'Game not found.', 'Press the link to search for it yourself.', 'https://howlongtobeat.com/', '012')
+
+		except Exception:
+			await error_message(message.channel, "Unknown Error: Google has probably blocked us, or it's currently down.", "Wait an hour and try again or inform the bot's developer.", '009')
+
+@client.event
+async def error_message(channel, error, solution, code):
+	embed = discord.Embed(title=error, description='SOLUTION: '+solution)
+	embed.set_author(name='RIP', icon_url='https://i.imgur.com/gs99PYz.png')
+	embed.set_footer(text='Error code: '+code)
+	await channel.send(embed=embed)
+
+async def error_message_with_url(channel, error, solution, url, code):
+	embed = discord.Embed(title=error, description='SOLUTION: '+solution, url=url)
+	embed.set_author(name='RIP', icon_url='https://i.imgur.com/gs99PYz.png')
+	embed.set_footer(text='Error code: '+code)	
+	await channel.send(embed=embed)
+
+@client.event
+async def on_ready():
+	await client.change_presence(activity=discord.Game(name="2.0 Now live", type=0))
+	print('Logged in as {0.user}'.format(client))
+	print(client.user.id)
+	print('------')
+
+client.run(TOKEN)
---
+++ website_data.py
@@ -0,0 +1,240 @@
+import string
+import urllib
+import random
+import re
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen, Request
+from googlesearch import search
+
+AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+
+def GetUserSoup(url): # Getting user profile page
+	r = Request(url, headers={'User-Agent': AGENT})
+	return BeautifulSoup(urlopen(r).read(), 'html5lib')
+
+class UserInfo(): # Analysing user profile
+
+	def __init__(self, soup):
+		self.s = soup
+
+	def name(self):
+		return self.s.find('div', {'class':'ellipsis'}).get_text(strip=True)
+
+	def card(self):
+		e = '.png$' + ''.join(random.choice(string.ascii_lowercase) for x in range(6)) # Generate a unique URL to prevent caching issues
+		return 'https://card.psnprofiles.com/1/' + self.s.find('div',{'class':'ellipsis'}).get_text(strip=True) + e
+
+	def icon(self):
+		return self.s.find('div', {'class':'avatar'}).find('img')['src']
+
+	def description(self):
+		try:
+			return '"'+self.s.find('span', {'class':'comment'}).get_text(strip=True)+'"'
+		except Exception:
+			return # In case there is no description
+
+class PlatinumInfo():
+
+	def __init__(self, soup):
+		self.s = soup
+
+	def name(self):
+		return self.s.find('div', {'class':'ellipsis'}).get_text(strip=True)
+
+	def game(self):
+		return self.s.find('tr',{'class':'platinum'}).find('a',{'class':'title'}).get_text(' ', strip=True)
+	
+	def image(self):
+		return self.s.find('tr',{'class':'platinum'}).img['src']
+
+	def description(self):
+		L1 = self.s.find('tr',{'class':'platinum'}).find_all('div',{'class':'small-info'})[0].get_text(' ', strip=True) + '\n'
+		L2 = self.s.find('tr',{'class':'platinum'}).find_all('div',{'class':'small-info'})[1].get_text(' ', strip=True)
+		L2 = re.sub(r'\s+', ' ', L2)
+		return L1+L2
+
+	def rarity(self):
+		return self.s.find('tr',{'class':'platinum'}).find('span',{'title':'Platinum Rarity'}).get_text(' ', strip=True)
+
+def GetTrophiesSoup(game): # Utilizing Google search for best results
+	query = f'{game} Trophies • PSNProfiles.com'
+	for result in search(query, tld='com', lang='en', num=1, start=0, stop=1, pause=1):
+		return BeautifulSoup(urlopen(result).read(), 'html5lib')
+
+class TrophiesInfo(): # Analysing trophies page
+
+	def __init__(self, soup):
+		self.s = soup
+
+	def Url(self):
+		return 'https://psnprofiles.com' + self.s.find('ul',{'class':'navigation'}).find('a')['href']
+
+	def Name(self):
+		return self.s.find('span', {'class':'breadcrumb-arrow'}).next_sibling
+
+	def Trophies(self):
+		return self.s.find('td', {'style':'padding: 10px'}).span.get_text(' ', strip=True)
+
+	def Image(self):
+		A = self.s.find('div', {'id':'first-banner'}).find('div',{'class':'img'})['style']
+		return A[A.find('(')+1 : A.find(')')]
+
+	def Comp(self): # The most complicated one since the completion is often displayed differently between games
+		B = self.s.find('span', text='100% Completed').previous_sibling
+		
+		if self.s.find('li', {'class':'icon-sprite platinum'}).get_text() == '1':
+			A = self.s.find('span', text='Platinum Achievers').previous_sibling
+		
+		if self.s.find('li', {'class':'icon-sprite platinum'}).get_text() == '0':
+			A = '0'
+				
+		if A[A.find('(')+1 : A.find(')')] == B[B.find('(')+1 : B.find(')')]:
+			A = A[A.find('(')+1 : A.find(')')]
+			return f' • {A}'
+		
+		if '(' in A and '(' in B:
+			A = A[A.find('(')+1 : A.find(')')]
+			B = B[b.find('(')+1 : B.find(')')]
+			return f' • {A} ({B})'
+		
+		if '(' in A and '(' not in B:
+			B = A[A.find('(')+1 : A.find(')')]
+			return f' • {B} (0%)'
+		
+		if '(' not in A and '(' in B:
+			B = B[B.find('(')+1 : B.find(')')]
+			return f' • {B}'
+		
+		else:
+			return ' • 0%' # In case no one 100%'d nor platinum'd the game
+
+	def Guide(self):
+		try:
+			url = 'https://psnprofiles.com' + self.s.find('ul',{'class':'navigation'}).find('a')['href'].replace('trophies','guides')
+			searchsoup = BeautifulSoup(urlopen(url).read(), 'html5lib')
+			url2 = 'https://psnprofiles.com'+searchsoup.find('div',{'class':'guide-page-info'}).find('a')['href']
+			guidesoup = BeautifulSoup(urlopen(url2).read(), 'html5lib')
+			dif = guidesoup.find('div',{'class':'overview-info'}).find_all('span')[0].get_text(' ', strip=True)
+			plays = guidesoup.find('div',{'class':'overview-info'}).find_all('span')[3].get_text(' ', strip=True)
+			hours = guidesoup.find('div',{'class':'overview-info'}).find_all('span')[6].get_text(' ', strip=True)
+			return dif + ' | ' + plays + ' | ' + hours
+			
+		except Exception:
+			return '' # In case there is no guide
+
+def FindGame(game): # Getting game price page
+	url = 'https://psprices.com/region-us/search/?q={}&platform=PS4&dlc=hide'.format(game.replace(' ','+'))
+	r = Request(url, headers={'User-Agent': AGENT})
+	page = BeautifulSoup(urlopen(r).read(), 'html5lib')
+	if page.find('div', {'class':'content__cat__pre_title'}) != None:
+		result = 'https://psprices.com'+page.find('div',{'class':'col-md-2 col-sm-3 col-xs-6'}).a['href']
+		r2 = Request(result, headers={'User-Agent': AGENT})
+		return BeautifulSoup(urlopen(r2).read(), 'html5lib')
+	else:
+		return page
+
+class PriceInfo(): # Analysing game price page
+
+	def __init__(self, soup):
+		self.s = soup
+	
+	def page_url(self):
+		return self.s.find('meta', {'property':'og:url'})['content']
+
+	def title(self):
+		return self.s.find('div', {'class':'col-md-9'}).find('h1').contents[0].strip()
+	
+	def StoreUrl(self):
+		return self.s.find('div', {'class':'col-xs-12 col-sm-6'}).find('a')['href']
+
+	def Price(self):
+		return self.s.find('div', {'class':'col-xs-12 col-sm-6'}).find('span',{'class':'current'}).get_text(strip=True)
+
+	def PlusPrice(self):
+		try:
+			A = self.A.find('div', {'class':'col-xs-12 col-sm-6'}).find('span',{'class':'plus'}).get_text(strip=True)
+			return f' ({A})'
+		except Exception:
+			return '' # In case there is no PLus Price
+	
+	def LowestPrice(soup):
+		return self.s.find('div', {'id':'price_history'}).strong.next_sibling.next_sibling.get_text(strip=True)
+
+	def Image(soup):
+		return self.s.find('div', {'class':'content__game_card__cover'}).find('img')['data-src']
+
+def FindMetaSoup(game): # Utilizing Google search for best results
+	query = 'site:metacritic.com/game {}'.format(game.replace(' ','+'))
+	for result in search(query, tld='com', lang='en', num=1, start=0, stop=1, pause=1):
+		r = Request(result, headers={'User-Agent': AGENT})
+		return BeautifulSoup(urlopen(r).read(), 'html5lib')
+
+###	SEARCH THROUGH METACRITIC DIRECTLY:
+#	url = 'https://www.metacritic.com/search/game/{}/results'.format(game.replace(' ','+'))
+#	r = Request(url, headers={'User-Agent': AGENT})
+#	page = BeautifulSoup(urlopen(r).read(), 'html5lib')
+#	result = 'https://www.metacritic.com'+page.find('h3',{'class': 'product_title basic_stat'}).a['href']
+#	r2 = Request(result, headers={'User-Agent': AGENT})
+#	return BeautifulSoup(urlopen(r2).read(), 'html5lib')
+
+class MetaInfo(): # Analysing Metacritic page
+
+	def __init__(self, soup):
+		self.s = soup
+
+	def title(self):
+		return self.s.find('div',{'class':'product_title'}).h1.get_text(strip=True)
+
+	def url(self):
+		return 'https://www.metacritic.com' + self.s.find('div',{'class':'product_title'}).a['href']
+
+	def image(self):
+		return self.s.find('img',{'class':'product_image large_image'})['src']
+
+	def score(self):
+		return "**" + self.s.find('span',{'itemprop':'ratingValue'}).get_text(strip=True) + "**"
+
+	def critics(self):
+		return self.s.find('span',{'class':'based'}).next_element.next_element.next_element.get_text(' ', strip=True)
+
+	def quote(self):
+		return '**Best**: "'+self.s.find('li',{'class':'review critic_review first_review'}).find('div',{'class':'review_body'}).get_text(strip=True)+'"\n **Worst**: "'+self.s.find('li',{'class':'review critic_review last_review'}).find('div',{'class':'review_body'}).get_text(strip=True)+'"'
+
+	def color(self):
+		X = int(self.s.find('span',{'itemprop':'ratingValue'}).get_text(strip=True))
+		if X >= 75:
+			return 0x66CC33
+		if X <= 74 and X >= 50:
+			return 0xffcc33
+		if X <= 49:
+			return 0xFF0000
+
+def HowLongSoup(game): # Utilizing Google search for best results
+	query = 'site:howlongtobeat.com {}'.format(game.replace(' ','+'))
+	for result in search(query, tld='com', lang='en', num=1, start=0, stop=1, pause=1):
+		r = Request(result, headers={'User-Agent': AGENT})
+		return BeautifulSoup(urlopen(r).read(), 'html5lib')
+
+class HowLongInfo(): # Analysing HowLongToBeat page
+
+	def __init__(self, soup):
+		self.s = soup
+
+	def title(self):
+		return self.s.find('div',{'class':'profile_header shadow_text'}).get_text(' ', strip=True)
+
+	def url(self):
+		return 'https://howlongtobeat.com/' + self.s.find('div',{'class':'content_75_static scrollable back_clear'}).a['href']
+
+	def image(self):
+		return self.s.find('img',{'alt':'Box Art'})['src']
+
+	def times(self): # Find all "game_times" classes and display them line after line
+		X = ''
+		for item in self.s.find('div',{'class':'game_times'}).find_all('li'):
+			X += str(item.find_all('h5'))+': '
+			X += '**'+str(item.find_all('div'))+'**\n'
+
+		X = BeautifulSoup(x, 'html5lib').get_text('', strip=True).replace('[','').replace(']','')
+		return X
